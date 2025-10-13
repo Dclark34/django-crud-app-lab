a0d972bc387249ce369e045245e0f5a3bca63c56
DClark <duncan.clark528@gmail.com>
2025-10-13 17:27:40 -0400

    added index and some css

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('parks/', views.park_index, name='park-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+#park class
+
+class Park:
+    def __init__(self, name, state, description, rating):
+        self.name = name
+        self.state = state
+        self.description = description
+        self.rating = rating
+
+
+parks = [
+    Park('Glacier National Park', 'Montana', 'Beautiful scenery and plenty of wildlife!', 10),
+    Park('Arches National Park', 'Utah', 'Awesome Geography, but too many lines', 6),
+    Park('Arcadia National Park', 'Maine', 'Great hiking. Beautiful in Fall', 8),
+    Park('Crater Lake National Park', 'Oregon', 'The crater is super cool!', 9),
+]
+
 #homepage
 def home(request):
-    return HttpResponse('<h1>Welcome to The National Parks Tracker!</h1>')
+    return render(request, 'home.html')
 
 #about route
 def about(request):
     return render(request, 'about.html')
+
+#parks index
+def park_index(request):
+    return render(request, 'parks/index.html', {'parks': parks})
