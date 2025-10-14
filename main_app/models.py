8e52b3e892b36fe3609948f1971b0875855a9e51
DClark <duncan.clark528@gmail.com>
2025-10-14 17:01:55 -0400

    added home.html

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Park(models.Model):
+        name = models.CharField(max_length=100)
+        state = models.CharField(max_length=20)
+        description = models.TextField(max_length = 250)
+        rating = models.IntegerField()
+
+        def __str__(self):
+                return self.name
